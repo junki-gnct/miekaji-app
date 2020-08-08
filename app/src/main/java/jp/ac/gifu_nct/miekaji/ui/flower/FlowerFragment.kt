febdcfb84a8a9d2b1bd6835a7b5ec6f99c87155b
satotakao <46733063+satotakao@users.noreply.github.com>
2020-08-09 01:39:10 +0900

    タイトル画面(仮)
    ボトムナビゲーションも作った

---
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/ui/flower/FlowerFragment.kt
@@ -0,0 +1,31 @@
+package jp.ac.gifu_nct.miekaji.ui.flower
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import jp.ac.gifu_nct.miekaji.R
+
+class FlowerFragment : Fragment() {
+
+    private lateinit var flowerViewModel: FlowerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        flowerViewModel =
+            ViewModelProviders.of(this).get(FlowerViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_flowers, container, false)
+        val textView: TextView = root.findViewById(R.id.text_flowers)
+        flowerViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
