ae88d38c992b87abe2930cc8aa7a91f1deb04b8c
satotakao <46733063+satotakao@users.noreply.github.com>
2020-09-04 22:05:11 +0900

    #1 トップ画面(タイトル画面)

--- app/src/main/java/jp/ac/gifu_nct/miekaji/ui/home/HomeFragment.kt
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package jp.ac.gifu_nct.miekaji.ui.home
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import jp.ac.gifu_nct.miekaji.R
+import jp.ac.gifu_nct.miekaji.ui.housework.HouseworkFragment
 
 class HomeFragment : Fragment() {
 
@@ -27,5 +31,18 @@ class HomeFragment : Fragment() {
             textView.text = it
         })*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val ToWork=view.findViewById<Button>(R.id.toHouseWork)
+        ToWork.setOnClickListener {
+            Log.d(TAG,"ToWork pressed")
+            val HworkFragment=HouseworkFragment()
+            val fragmentTransaction=fragmentManager?.beginTransaction()
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.replace(R.id.nav_host_fragment,HworkFragment)
+            fragmentTransaction?.commit()
+        }
     }
 }
