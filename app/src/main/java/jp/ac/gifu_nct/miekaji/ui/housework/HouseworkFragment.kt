7474e2324777bc4004c59654e4f6fe237ec2ff73
satotakao <46733063+satotakao@users.noreply.github.com>
2020-09-08 12:16:47 +0900

    #2 測定画面(結果)(仮)

--- app/src/main/java/jp/ac/gifu_nct/miekaji/ui/housework/HouseworkFragment.kt
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/ui/housework/HouseworkFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import jp.ac.gifu_nct.miekaji.R
+import kotlinx.android.synthetic.main.fragment_housework.*
 
 class HouseworkFragment : Fragment() {
 
@@ -22,9 +24,9 @@ class HouseworkFragment : Fragment() {
         houseworkboardViewModel =
             ViewModelProviders.of(this).get(HouseworkViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_housework, container, false)
-        val textView: TextView = root.findViewById(R.id.text_housework)
+        val Wvalue: TextView = root.findViewById(R.id.workValue)
         houseworkboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            Wvalue.text = "300"
         })
         return root
     }
