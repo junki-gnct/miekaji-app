33a01d83612408bf67c38b1bc13efb5e0ac0f737
satotakao <46733063+satotakao@users.noreply.github.com>
2020-09-16 21:14:27 +0900

    Revert "タイトル画面(仮)"

--- app/src/main/java/jp/ac/gifu_nct/miekaji/MainActivity.kt
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/MainActivity.kt
@@ -2,30 +2,10 @@ package jp.ac.gifu_nct.miekaji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_housework,
-                R.id.navigation_flower,
-                R.id.navigation_friend
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
