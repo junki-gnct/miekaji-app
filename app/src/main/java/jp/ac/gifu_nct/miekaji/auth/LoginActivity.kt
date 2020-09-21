5fe87090093836b77385e66df8128587afd16851
Junki Tomatsu <2018e25@edu.gifu-nct.ac.jp>
2020-09-21 16:17:22 +0900

    ログイン/新規登録用Activityの追加

---
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/auth/LoginActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.gifu_nct.miekaji.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/auth/SignupActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.gifu_nct.miekaji.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
