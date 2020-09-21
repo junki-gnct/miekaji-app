e808f39c2d9c81b68f8435b85947d76323c5df9b
Junki Tomatsu <2018e25@edu.gifu-nct.ac.jp>
2020-09-21 19:48:52 +0900

    4xx, 5xxエラー時にアプリが落ちる問題を修正

--- app/src/main/java/jp/ac/gifu_nct/miekaji/utils/http/HTTPClient.kt
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/utils/http/HTTPClient.kt
@@ -2,10 +2,7 @@ package jp.ac.gifu_nct.miekaji.utils.http
 
 import android.util.Log
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
@@ -13,6 +10,7 @@ import java.net.URLEncoder
 object HTTPClient {
 
     fun getRequest(url: String, parameters: HashMap<String, String>?): JSONObject {
+        var result: String = ""
         var arguments = ""
         if (parameters != null) {
             parameters!!.keys.forEach{
@@ -29,51 +27,90 @@ object HTTPClient {
 
         val url = URL(requestedUrl);
         val con = url.openConnection() as HttpURLConnection
-        con.requestMethod = "GET"
-        con.useCaches = false
-        con.doOutput = true
-        con.doInput = true
-        con.connect()
+        try {
+            con.requestMethod = "GET"
+            con.useCaches = false
+            con.doOutput = true
+            con.doInput = true
+            con.connect()
 
-        val br = BufferedReader(InputStreamReader(con.inputStream))
+            val br = BufferedReader(InputStreamReader(con.inputStream))
 
-        var line: String? = null
-        val sb = StringBuilder()
+            var line: String? = null
+            val sb = StringBuilder()
 
-        for (line in br.readLines()) {
-            line?.let { sb.append(line) }
+            for (line in br.readLines()) {
+                line?.let { sb.append(line) }
+            }
+
+            br.close()
+
+            result = sb.toString()
+        } catch(e: FileNotFoundException) {
+            val br = BufferedReader(InputStreamReader(con.errorStream))
+
+            var line: String? = null
+            val sb = StringBuilder()
+
+            for (line in br.readLines()) {
+                line?.let { sb.append(line) }
+            }
+
+            br.close()
+
+            result = sb.toString()
+        } catch(e: IOException) {
+            e.printStackTrace()
         }
 
-        br.close()
-
-        return JSONObject(sb.toString())
+        return JSONObject(result)
     }
 
     fun postRequest(url: String, parameters: HashMap<String, String>?): JSONObject {
+        var result = ""
         val url = URL(url);
         val con = url.openConnection() as HttpURLConnection
-        con.requestMethod = "POST"
-        con.doOutput = true
-        con.doInput = true
-        con.useCaches = false
-        con.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-        con.connect()
+        try {
+            con.requestMethod = "POST"
+            con.doOutput = true
+            con.doInput = true
+            con.useCaches = false
+            con.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            con.connect()
 
-        val bw = BufferedWriter(OutputStreamWriter(con.outputStream))
-        bw.write(JSONObject(parameters as Map<String, String>).toString())
-        bw.close()
+            val bw = BufferedWriter(OutputStreamWriter(con.outputStream))
+            bw.write(JSONObject(parameters as Map<String, String>).toString())
+            bw.close()
 
-        val br = BufferedReader(InputStreamReader(con.inputStream))
+            val br = BufferedReader(InputStreamReader(con.inputStream))
 
-        var line: String? = null
-        val sb = StringBuilder()
+            var line: String? = null
+            val sb = StringBuilder()
 
-        for (line in br.readLines()) {
-            line?.let { sb.append(line) }
+            for (line in br.readLines()) {
+                line?.let { sb.append(line) }
+            }
+
+            br.close()
+
+            result = sb.toString()
+        } catch(e: FileNotFoundException) {
+            val br = BufferedReader(InputStreamReader(con.errorStream))
+
+            var line: String? = null
+            val sb = StringBuilder()
+
+            for (line in br.readLines()) {
+                line?.let { sb.append(line) }
+            }
+
+            br.close()
+
+            result = sb.toString()
+        } catch(e: IOException) {
+            e.printStackTrace()
         }
 
-        br.close()
-
-        return JSONObject(sb.toString())
+        return JSONObject(result)
     }
 }
