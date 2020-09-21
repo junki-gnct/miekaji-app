608739f2e420643ad753774318174148bc1f226c
Junki Tomatsu <2018e25@edu.gifu-nct.ac.jp>
2020-09-21 16:17:46 +0900

    エラー解消

--- app/src/main/java/jp/ac/gifu_nct/miekaji/auth/SignupActivity.kt
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/auth/SignupActivity.kt
@@ -2,6 +2,7 @@ package jp.ac.gifu_nct.miekaji.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.gifu_nct.miekaji.R
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
