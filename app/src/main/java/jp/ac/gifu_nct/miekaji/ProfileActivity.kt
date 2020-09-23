4ab296463fbd2aa7ba1a8047ee9593debe038fb4
Junki Tomatsu <2018e25@edu.gifu-nct.ac.jp>
2020-09-24 00:31:00 +0900

    WIP

---
+++ app/src/main/java/jp/ac/gifu_nct/miekaji/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.gifu_nct.miekaji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
